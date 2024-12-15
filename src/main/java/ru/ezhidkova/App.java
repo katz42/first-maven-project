ccb3aa65e498cf2bfd1dea6df4ef53b8b2c26cc8
ekaterina <zhidkova.ea@gmail.com>
2024-12-15 22:40:33 +0300

    pom fix

--- src/main/java/ru/ezhidkova/App.java
+++ src/main/java/ru/ezhidkova/App.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main(String[] args ){
+    public static void main (String[] args ){
         System.out.println("Введите название дня недели на английском:");
         Scanner scanner = new Scanner(System.in);
         String day = scanner.nextLine();
